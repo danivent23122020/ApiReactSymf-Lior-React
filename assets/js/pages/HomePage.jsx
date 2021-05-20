782b2b0ff9d1915b9a6087d627d7b84fb7afbb43
Danivent <dan.vent@laposte.net>
2021-05-20 07:17:52 +0200

    homePage

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -8,12 +8,12 @@ const HomePage = props => {
         className="list-group-item list-group-item-action flex-column align-items-start active"
       >
         <div className="d-flex w-100 justify-content-between">
-          <h5 className="mb-1">List group item heading</h5>
+          <h4 className="mb-1">INVOICES MANAGER</h4>
           <small>3 days ago</small>
         </div>
         <p className="mb-1">
-          Donec id elit non mi porta gravida at eget metus. Maecenas sed diam
-          eget risus varius blandit.
+          Bonjour et bienvenue sur mon site de gestion de factures avec Symfony,
+          API Platform et React
         </p>
         <small>Donec id elit non mi porta.</small>
       </a>

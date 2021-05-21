dfad90e4874246c505a99375ca87a565676dd822
Danivent <dan.vent@laposte.net>
2021-05-21 09:48:20 +0200

    Css-homepage

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomePage = props => {
   return (
-    <div className="list-group d-flex align-items-center mt-5">
+    <div className="list-group d-flex align-items-center mt-5 homepage p-5">
       <div className="col-5 d-flex flex-column align-items-center">
         <h1 className="mb-1">INVOICES MANAGER</h1>
         <h3 className="mt-5">
